720aa5d98fe4fcd30ce202c45102ab344b54ccbb
ando07 <ando.masahito@gmail.com>
2019-10-26 10:42:38 +0900

    [modify]create base scene

--- arkit_model3d/arkit_model3d/ViewController.swift
+++ arkit_model3d/arkit_model3d/ViewController.swift
@@ -20,6 +20,18 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Set the view's delegate
         sceneView.delegate = self
         
+        // create and register scene
+        sceneView.scene = SCNScene()
         
+        // view feature points
+        sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints]
+        
+        // add light
+        sceneView.autoenablesDefaultLighting = true
+        
+        // detection horizontal plane
+        let configlation = ARWorldTrackingConfiguration()
+        configlation.planeDetection = .horizontal
+        sceneView.session.run(configlation)
     }
 }
