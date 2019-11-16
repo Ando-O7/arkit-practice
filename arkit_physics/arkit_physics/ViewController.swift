0d278a298783c8ce93cd1fba2a2885ea163a20eb
ando07 <ando.masahito@gmail.com>
2019-11-16 20:09:55 +0900

    [add]create and add child with node

--- arkit_physics/arkit_physics/ViewController.swift
+++ arkit_physics/arkit_physics/ViewController.swift
@@ -48,5 +48,22 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // add node
         sceneView.scene.rootNode.addChildNode(sphereNode)
     }
+    
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        guard let planeAnchor = anchor as? ARPlaneAnchor else {fatalError()}
+        
+        // create Node
+        let planeNode = SCNNode()
+        
+        // create geometry
+        let geometry = SCNPlane(width: CGFloat(planeAnchor.extent.x), height: CGFloat(planeAnchor.extent.z))
+        geometry.materials.first?.diffuse.contents = UIColor.black.withAlphaComponent(0.5)
+        
+        // setting geometry and transform
+        planeNode.geometry = geometry
+        planeNode.transform = SCNMatrix4MakeRotation(-Float.pi / 2.0, 1, 0, 0)
+        
+        node.addChildNode(planeNode)
+    }
 
 }
