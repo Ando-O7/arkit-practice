60882cde779ae1ba517ff2f7670b832cf2eb9012
ando07 <ando.masahito@gmail.com>
2019-11-17 21:17:15 +0900

    [add]update geometry with didUpdate

--- arkit_physics/arkit_physics/ViewController.swift
+++ arkit_physics/arkit_physics/ViewController.swift
@@ -65,5 +65,16 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         node.addChildNode(planeNode)
     }
+    
+    func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
+        guard let planeAnchor = anchor as? ARPlaneAnchor else {fatalError()}
+        guard let geometryPlaneNode = node.childNodes.first,
+            let planeGeometry = geometryPlaneNode.geometry as? SCNPlane else {fatalError()}
+        
+        // update Geometry
+        planeGeometry.width = CGFloat(planeAnchor.extent.x)
+        planeGeometry.height = CGFloat(planeAnchor.extent.z)
+        geometryPlaneNode.simdPosition = SIMD3(planeAnchor.center.x, 0, planeAnchor.center.z)
+    }
 
 }
