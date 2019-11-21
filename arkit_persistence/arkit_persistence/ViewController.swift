fd1ed9432247abd40a60cf028a0c18b68f7090a3
ando07 <ando.masahito@gmail.com>
2019-11-22 08:47:44 +0900

    [add]add worldMapURL

--- arkit_persistence/arkit_persistence/ViewController.swift
+++ arkit_persistence/arkit_persistence/ViewController.swift
@@ -14,6 +14,19 @@ class ViewController: UIViewController, ARSCNViewDelegate {
 
     @IBOutlet var sceneView: ARSCNView!
     
+    var worldMapURL: URL = {
+        do {
+            return try FileManager.default.url(
+                for: .documentDirectory,
+                in: .userDomainMask,
+                appropriateFor: nil,
+                create: true
+            ).appendingPathComponent("WorldMapURL")
+        } catch {
+            fatalError("No such file")
+        }
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
