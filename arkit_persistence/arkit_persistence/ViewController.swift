4ab1299d009e4298228ca2a3c5f41c08c468f960
ando07 <ando.masahito@gmail.com>
2019-11-24 21:24:08 +0900

    [add]create scene - view feature points - detection horizontal plane

--- arkit_persistence/arkit_persistence/ViewController.swift
+++ arkit_persistence/arkit_persistence/ViewController.swift
@@ -62,5 +62,20 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Set the view's delegate
         sceneView.delegate = self
+        
+        // create and register scene
+        sceneView.scene = SCNScene()
+        
+        // view feature points
+        sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints]
+        
+        // add light
+        sceneView.autoenablesDefaultLighting = true
+        
+        // detection plane
+        let configuration = ARWorldTrackingConfiguration()
+        configuration.planeDetection = .horizontal
+        
+        sceneView.session.run(configuration)
     }
 }
