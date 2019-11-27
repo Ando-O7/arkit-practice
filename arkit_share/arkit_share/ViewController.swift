3d2a9dca3e422c2e132541e05d07b20aeca6efd7
ando07 <ando.masahito@gmail.com>
2019-11-28 08:36:39 +0900

    [add]setting color process

--- arkit_share/arkit_share/ViewController.swift
+++ arkit_share/arkit_share/ViewController.swift
@@ -14,9 +14,14 @@ import MultipeerConnectivity
 class ViewController: UIViewController, ARSCNViewDelegate {
 
     @IBOutlet var sceneView: ARSCNView!
+    let colorTable = [UIColor.red, UIColor.orange, UIColor.yellow, UIColor.green, UIColor.blue, UIColor.purple, UIColor.white, UIColor.black]
+    var myColorIdx = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // setting color
+        myColorIdx = Int(arc4random_uniform(UInt32(colorTable.count)))
         
         // Set the view's delegate
         sceneView.delegate = self
