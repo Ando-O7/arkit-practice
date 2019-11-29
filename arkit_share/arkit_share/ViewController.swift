d9204009d34e9f340cb178bc006542ed3a4cee8b
ando07 <ando.masahito@gmail.com>
2019-11-29 22:50:43 +0900

    [add]function receivedData

--- arkit_share/arkit_share/ViewController.swift
+++ arkit_share/arkit_share/ViewController.swift
@@ -14,6 +14,7 @@ import MultipeerConnectivity
 class ViewController: UIViewController, ARSCNViewDelegate {
 
     @IBOutlet var sceneView: ARSCNView!
+    var otherPeerID: MCPeerID?
     let colorTable = [UIColor.red, UIColor.orange, UIColor.yellow, UIColor.green, UIColor.blue, UIColor.purple, UIColor.white, UIColor.black]
     var myColorIdx = 0
     
@@ -25,5 +26,25 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Set the view's delegate
         sceneView.delegate = self
+    }
+    
+    func receivedData(_ data: Data, from peer: MCPeerID) {
+        do {
+            // if receive data is ARWorldMap
+            if let worldMap = try NSKeyedUnarchiver.unarchivedObject(ofClass: ARWorldMap.self, from: data) {
+                // run the session with the received world map
+                let configuration = ARWorldTrackingConfiguration()
+                configuration.planeDetection = .horizontal
+                configuration.initialWorldMap = worldMap
+                sceneView.session.run(configuration, options: [.resetTracking, .removeExistingAnchors])
+                otherPeerID = peer
+            }
+        } catch {}
+        do {
+            // if receive data is ARAnchor
+            if let anchor = try NSKeyedUnarchiver.unarchivedObject(ofClass: ARAnchor.self, from: data) {
+                sceneView.session.add(anchor: anchor)
+            }
+        } catch {}
     }
 }
