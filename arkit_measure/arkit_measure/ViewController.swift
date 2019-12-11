7e1f8530e2e7672847b1bb60960474c50f5ea6a1
ando07 <ando.masahito@gmail.com>
2019-12-11 13:14:20 +0900

    [add]create scene

--- arkit_measure/arkit_measure/ViewController.swift
+++ arkit_measure/arkit_measure/ViewController.swift
@@ -13,11 +13,22 @@ import ARKit
 class ViewController: UIViewController, ARSCNViewDelegate {
 
     @IBOutlet var sceneView: ARSCNView!
+    var centerPos = CGPoint(x: 0, y: 0)
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Set the view's delegate
         sceneView.delegate = self
+
+        // create scene
+        sceneView.scene = SCNScene()
+
+        // save center position
+        centerPos = sceneView.center
+
+        // start session
+        let configuration = ARWorldTrackingConfiguration()
+        sceneView.session.run(configuration)
     }
 }
