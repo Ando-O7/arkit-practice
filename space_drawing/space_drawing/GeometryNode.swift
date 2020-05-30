fdf942ebb0d6e578422b5a5f45264a5e8dbd79bb
ando07 <ando.masahito@gmail.com>
2020-05-30 18:03:22 +0900

    [add]GeometryNode Initializa

--- space_drawing/space_drawing/GeometryNode.swift
+++ space_drawing/space_drawing/GeometryNode.swift
@@ -6,4 +6,20 @@
 //  Copyright © 2020 AND. All rights reserved.
 //
 
-import Foundation
+import SceneKit
+
+open class GeometryNode: SCNNode {
+
+    private let lineWidth: Float
+    private let color: UIColor
+
+    public init(color: UIColor, lineWidth: Float) {
+        self.color = color
+        self.lineWidth = lineWidth
+        super.init()
+    }
+
+    public required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
